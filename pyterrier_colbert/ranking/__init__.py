ae4bde19f77df4ebc0cf488430eaba5219707163
Craig Macdonald <craig.macdonald@glasgow.ac.uk>
2025-07-25 18:12:26 +0100

    refactor

---
+++ pyterrier_colbert/ranking/__init__.py
@@ -0,0 +1,3 @@
+from ._modelonly import ColBERTv2ModelOnly
+from ._index import ColBERTv2Index
+
---
+++ pyterrier_colbert/ranking/_index.py
@@ -0,0 +1,53 @@
+from ._modelonly import ColBERTModelOnlyFactory
+
+import pandas as pd
+import pyterrier as pt
+
+from pyterrier import tqdm
+from colbert.evaluation.load_model import load_model
+from .. import load_checkpoint
+# monkeypatch to use our downloading version
+import colbert.evaluation.loaders
+colbert.evaluation.loaders.load_checkpoint = load_checkpoint
+colbert.evaluation.loaders.load_model.__globals__['load_checkpoint'] = load_checkpoint
+from colbert.searcher import Searcher
+from warnings import warn
+
+
+class ColBERTv2Index(ColBERTModelOnlyFactory, pt.Artifact):
+
+    def __init__(self, colbert, index_location, **kwargs):
+        # TODO do we need the colbert checkpoint....; Searcher will load it too.
+        super().__init__(colbert, **kwargs)
+        import os
+        dirs = os.path.split(index_location)
+        self.searcher = Searcher(dirs[-1], index_root=os.path.join(*dirs[0:-1]))
+        self.docno_mapping = {}
+
+        # Load the docno mappings from the permanent file
+        docno_file = os.path.join(index_location, "docnos.npids")
+        from npids import Lookup
+        self.docnos = Lookup(docno_file)
+
+
+    def end_to_end(self, k=100) -> pt.Transformer:
+        def _search(df_query):
+            if len(df_query) == 0:
+                return pd.DataFrame(columns=["qid", "docno", "score", "rank"])
+            
+            # TODO can we make df_queries into a colbert.Queries object
+            assert len(df_query) == 1
+            # encode Q
+            Q = self.searcher.encode([df_query.iloc[0]["query"]])
+      
+            # call colbert.Searcher
+            docids, ranks, scores = self.searcher.dense_search(Q, k=k)
+            docnos = self.docnos.fwd[docids]
+            assert len(docnos) == len(scores)
+            return pd.DataFrame({
+                "qid": [df_query.iloc[0]["qid"]] * len(docnos),
+                "docno": docnos,
+                "score": scores,
+                "rank": ranks
+            })
+        return pt.apply.by_query(_search, add_ranks=False)
