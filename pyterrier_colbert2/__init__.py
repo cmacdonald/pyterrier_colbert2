2e3ea3be37c50b3334aff8a5c15d8028b1ab7cde
Craig Macdonald <craig.macdonald@glasgow.ac.uk>
2026-07-03 10:27:14 +0100

    Refactor load_checkpoint function and imports

--- pyterrier_colbert2/__init__.py
+++ pyterrier_colbert2/__init__.py
@@ -1,8 +1,4 @@
 
-#import importlib
-#ranking = importlib.import_module('.' + 'indexing', package='pyterrier_colbert2') 
-#ranking = importlib.import_module('.' + 'ranking', package='pyterrier_colbert2')
-
 from . import ranking
 from . import indexing
 
