cc0fb17f54019edef45efa8713303ecc8239e39e
Xiao0728 <xiao.wang@uibe.edu.cn>
2026-09-15 17:08:06 +0800

    Preserve expanded query length for encoded PLAID search

--- pyterrier_colbert2/ranking/_index.py
+++ pyterrier_colbert2/ranking/_index.py
@@ -41,6 +41,7 @@ class ColBERTv2Index(ColBERTModelOnlyFactory, pt.Artifact):
         self.ndocs = ndocs
         dirs = os.path.split(index_location)
         self.searcher = Searcher(dirs[-1], index_root=os.path.join(*dirs[0:-1]))
+
         if self.plaid_mode:
             self.searcher.configure(ncells=self.ncells,
                                 centroid_score_threshold=self.centroid_score_threshold,
@@ -74,8 +75,22 @@ class ColBERTv2Index(ColBERTModelOnlyFactory, pt.Artifact):
                 Q = Q.cuda()
 
             # call colbert.Searcher or plaid if plaid_mode is True
-            docids, ranks, scores = self.searcher.dense_search(Q, k=k)
+            # docids, ranks, scores = self.searcher.dense_search(Q, k=k)
+            # docnos = self.docnos.fwd[docids]
+
+            # let PLAID candidate generation see all query vectors
+            original_query_maxlen = self.searcher.config.query_maxlen
+            
+            try:
+                if self.plaid_mode:# and query_encoded:
+                    self.searcher.config.query_maxlen = int(Q.size(-2))
+            
+                docids, ranks, scores = self.searcher.dense_search(Q, k=k)
+            finally:
+                self.searcher.config.query_maxlen = original_query_maxlen
+            
             docnos = self.docnos.fwd[docids]
+            
 
             # ignore the ranks returned by the searcher and re-assign them based on the sorted order of scores, 
             # to ensure consistency between colbertv2 and plaid modes. This is because in plaid mode, the searcher 
